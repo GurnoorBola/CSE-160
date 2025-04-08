fb4d8a9af36712b4511471233162665425b89633
gurnoor <gurnoor.bola@gmail.com>
2025-04-08 15:56:34 -0700

    Finished up to step 8

--- asg1/asg1.js
+++ asg1/asg1.js
@@ -2,9 +2,11 @@
 // Vertex shader program
 var VSHADER_SOURCE = `
   attribute vec4 a_Position;
+  uniform float u_Size;
   void main() {
     gl_Position = a_Position;
-    gl_PointSize = 10.0;
+    //gl_PointSize = 10.0;
+    gl_PointSize = u_Size;
   }`
 
 // Fragment shader program
@@ -20,13 +22,14 @@ let canvas;
 let gl;
 let a_Position;
 let u_FragColor;
+let u_Size;
 
 function setupWegbGL(){
   // Retrieve <canvas> element
   canvas = document.getElementById('webgl');
 
   // Get the rendering context for WebGL
-  gl = getWebGLContext(canvas);
+  gl = canvas.getContext('webgl', { preserveDrawingBuffer: true});
   if (!gl) {
     console.log('Failed to get the rendering context for WebGL');
     return;
@@ -53,9 +56,18 @@ function connectVariablesToGLSL(){
     console.log('Failed to get the storage location of u_FragColor');
     return;
   }
+
+  //Get the storage location of u_Size
+  u_Size = gl.getUniformLocation(gl.program, 'u_Size');
+  if (!u_Size) {
+    console.log('Failed to get the storage location of u_Size');
+    return;
+  }
 }
 
+//Globals related UI elemenents
 let g_selectedColor = [1.0, 1.0, 1.0, 1.0]
+let g_selectedSize = 5;
 
 //set up actions for the HTML UI elements
 function addActionsForHtmlUI(){
@@ -63,43 +75,40 @@ function addActionsForHtmlUI(){
   //Button Events (Shape Type)
   document.getElementById('green').onclick = function () {g_selectedColor = [0.0, 1.0, 0.0, 1.0]; };
   document.getElementById('red').onclick = function () {g_selectedColor = [1.0, 0.0, 0.0, 1.0]; };
+  document.getElementById('clearButton').onclick = function() {g_shapesList=[]; renderAllShapes(); };
 
   document.getElementById('redSlide').addEventListener('mouseup', function() {g_selectedColor[0] = this.value/100; });
   document.getElementById('greenSlide').addEventListener('mouseup', function() {g_selectedColor[1] = this.value/100; });
   document.getElementById('blueSlide').addEventListener('mouseup', function() {g_selectedColor[2] = this.value/100; });
+
+  document.getElementById('sizeSlide').addEventListener('mouseup', function() {g_selectedSize = this.value; });
 }
 
-function main() {
-  
-  //set up canvas and gl variabls
-  setupWegbGL();
-  //set up actions for the HTML UI elements
-  connectVariablesToGLSL();
+var g_shapesList = [];
 
-  //set up actions for the HTML UI elements
-  addActionsForHtmlUI();
+// var g_points = [];  // The array for the position of a mouse press
+// var g_colors = [];  // The array to store the color of a point
+// var g_sizes = []; // The array to sotre the size of a point
 
-  // Register function (event handler) to be called on a mouse press
-  canvas.onmousedown = click;
-
-  // Specify the color for clearing <canvas>
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Clear <canvas>
-  gl.clear(gl.COLOR_BUFFER_BIT);
-}
-
-var g_points = [];  // The array for the position of a mouse press
-var g_colors = [];  // The array to store the color of a point
 function click(ev) {
   // extract the event click and return it in WebGL coordinates
   [x, y] = convertCoordinatesEventToGL(ev);
 
-  // Store the coordinates to g_points array
-  g_points.push([x, y]);
+  //Create and store the new point
+  let point = new Point();
+  point.position = [x, y];
+  point.color=g_selectedColor.slice();
+  point.size=g_selectedSize;
+  g_shapesList.push(point);
 
-  //store the color to g_colors array
-  g_colors.push(g_selectedColor.slice());
+  // // Store the coordinates to g_points array
+  // g_points.push([x, y]);
+
+  // //store the color to g_colors array
+  // g_colors.push(g_selectedColor.slice());
+
+  // //store the size to the g sizes array
+  // g_sizes.push(g_selectedSize);
 
   // // Store the coordinates to g_points array
   // if (x >= 0.0 && y >= 0.0) {      // First quadrant
@@ -127,19 +136,47 @@ function convertCoordinatesEventToGL(ev){
 
 //Draw every shape that is supposed to be in the canvas
 function renderAllShapes(){
+
+    //check the time at the start of this function
+    var startTime = performance.now();
     // Clear <canvas>
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var len = g_points.length;
+    // var len = g_points.length;
+    var len = g_shapesList.length;
     for(var i = 0; i < len; i++) {
-      var xy = g_points[i];
-      var rgba = g_colors[i];
-  
-      // Pass the position of a point to a_Position variable
-      gl.vertexAttrib3f(a_Position, xy[0], xy[1], 0.0);
-      // Pass the color of a point to u_FragColor variable
-      gl.uniform4f(u_FragColor, rgba[0], rgba[1], rgba[2], rgba[3]);
-      // Draw
-      gl.drawArrays(gl.POINTS, 0, 1);
+      g_shapesList[i].render();
     }
+
+    var duration = performance.now() - startTime;
+    sendTextToHTML('numdot: ' + len + ' ms: ' + Math.floor(duration) + ' fps: ' + Math.floor(10000/duration)/10, 'numdot');
+}
+
+function sendTextToHTML(text, htmlID){
+  var htmlElm = document.getElementById(htmlID);
+  if (!htmlElm) {
+    console.log('Failed to get ' + htmlID + " from HTML");
+  }
+  htmlElm.innerHTML = text;
+}
+
+function main() {
+  
+  //set up canvas and gl variabls
+  setupWegbGL();
+  //set up actions for the HTML UI elements
+  connectVariablesToGLSL();
+
+  //set up actions for the HTML UI elements
+  addActionsForHtmlUI();
+
+  // Register function (event handler) to be called on a mouse press
+  canvas.onmousedown = click;
+  canvas.onmousemove = function(ev) { if (ev.buttons == 1) { click (ev) } };
+
+  // Specify the color for clearing <canvas>
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  // Clear <canvas>
+  gl.clear(gl.COLOR_BUFFER_BIT);
 }
