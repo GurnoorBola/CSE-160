e97f10383c5d6ea688dff6f20365f015b63526cd
gurnoor <gurnoor.bola@gmail.com>
2025-05-09 13:33:49 -0700

    Fixed camera starting point

--- asg3/src/Camera.js
+++ asg3/src/Camera.js
@@ -2,8 +2,8 @@ let camCanvas = document.getElementById('webgl');
 class Camera{
     constructor (){
         this.fov = 60;
-        this.eye = new Vector3([0,0, 2]);
-        this.at = new Vector3([0, 0, 0]);
+        this.eye = new Vector3([0,1, -2]);
+        this.at = new Vector3([0, 1, 0]);
         this.up = new Vector3([0, 1, 0]);
         this.viewMatrix = new Matrix4().setLookAt(...this.eye.elements, ...this.at.elements, ...this.up.elements);
         this.projMatrix = new Matrix4().setPerspective(this.fov, camCanvas.width/camCanvas.height, 0.1, 1000);
